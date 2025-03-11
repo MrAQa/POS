454aec867ce408a2d7169a9e160eb7c54a62b08b
Abdul Qadeer <abdul.qadeer@iskaan.com>
2025-03-11 13:49:28 +0500

    new

--- material-kit-react-main/src/pages/ComponentToPrint.js
+++ material-kit-react-main/src/pages/ComponentToPrint.js
@@ -10,20 +10,20 @@ const formattedDateTime = now.format('DD-MM-YYYY h:mm A');
 const ComponentToPrint = React.forwardRef((props, ref) => {
     return (
         <>
-            <div ref={ref} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontSize: '12px' }}>
+            <div ref={ref} className='printRecept' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontSize: '12px'}}>
                 <h2 style={{ fontWeight: '700' }}>The Hunger Bites</h2>
                 {/* <img className='mainHeading' alt='logo' width={100} height={100} src={logo} /> */}
                 {' '}   {' '} <small style={{wordBreak:'break-word',paddingLeft:'10px',paddingRight:'10px',display: 'flex', alignItems: 'center',justifyContent:'center',textAlign:'center'}}>Main market opposite nadra office main zarrar shaheed road jorry pull lahore</small> {' '} {' '}
                 <span>#03298550193</span>
 
-                --------------------------------------------
+                -----------------------------
                 {/* <span>Order # </span>
                 -------------------------------------------- */}
 
                 <table>
                     <thead>
                         <tr>
-                            <th style={{ fontWeight: '600' }}>Title</th>
+                            <th>Title</th>
                             <th style={{ textAlign: 'center'}}>Qty</th>
                             <th>Price</th>
                         </tr>
@@ -41,7 +41,7 @@ const ComponentToPrint = React.forwardRef((props, ref) => {
 
                     </tbody>
                 </table>
-                ---------------------- ----------------------
+                -------------- --------------
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <span style={{ marginRight: '60px' }}>
                         Total {' '}{' '}{' '}{' '}{' '}{' '}{' '}{' '}{' '}
@@ -50,7 +50,7 @@ const ComponentToPrint = React.forwardRef((props, ref) => {
                         {props.calculateTotal}
                     </span>
                 </div>
-                ---------------------- ----------------------
+               -------------- --------------
 
                 <span className='mt-1'><b>Thank  you</b></span>
                 {/* <span>{formattedDateTime}</span> */}
